585f259a419d2f412ebb8cbe694e301676cd36b8
Nigth <omnes.theo@gmail.com>
2021-02-18 18:38:36 +0100

    fix view signup delivery man

--- views/View.php
+++ views/View.php
@@ -1,38 +1,43 @@
 <?php
 
-class View {
-  private $_file;
-  private $_header = '';
-  private $_css = [];
-  private $_js = [];
+class View
+{
+    private $_file;
+    private $_header = '';
+    private $_css = [];
+    private $_js = [];
 
-  public function __construct($action) {
-    $this->_file = 'views/view'. $action . '.php';
-  }
+    public function __construct($action)
+    {
+        $this->_file = 'views/view' . $action . '.php';
+    }
 
-  public function generateView($data) {
-    $content = $this->generateFile($this->_file, $data);
-    $view = $this->generateFile('views/template.php',
-      [ 'header' => $this->_header,
-        'content' => $content
-      ]);
+    public function generateView($data = null) {
+        $content = $this->generateFile($this->_file, $data);
+        $view = $this->generateFile('views/template.php',
+            ['header' => $this->_header,
+                'content' => $content
+            ]);
 
-    echo $view;
-  }
+        echo $view;
+    }
 
-  public function generateTemplate($file, $data) {
-    return $this->generateFile('templates/'.$file.'.php', $data);
-  }
+    public function generateTemplate($file, $data)
+    {
+        return $this->generateFile('templates/' . $file . '.php', $data);
+    }
 
-  private function generateFile( $file, $data ) {
-    if( file_exists($file) ) {
-      extract($data);
+    private function generateFile($file, $data=null)
+    {
+        if (file_exists($file)) {
+            if( $data != null )
+                extract($data);
 
-      ob_start();
-      require $file;
-      return ob_get_clean();
-    } else
-      throw new Exception("File " . $file . 'not found');
+            ob_start();
+            require $file;
+            return ob_get_clean();
+        } else
+            throw new Exception("File " . $file . 'not found');
 
-  }
+    }
 }

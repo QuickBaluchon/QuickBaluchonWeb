09b6f2390d94c55a184e3a8a8ec71f56d2b26b73
Nigth <omnes.theo@gmail.com>
2021-03-07 18:26:37 +0100

    add nb of kilometers

--- media/script/deliveryman/stats.js
+++ media/script/deliveryman/stats.js
@@ -4,6 +4,9 @@ window.onload = function () {
         deliveryman: 4
     }) ;
     ajax('../api/deliveryManStats/6', json, 'POST', displayStatsPackages, console.log) ;
+
+    displayStatsDeliveries();
+
 }
 
 function displayStatsPackages(stats) {
@@ -40,6 +43,59 @@ function displayStatsPackages(stats) {
                     ticks: {
                         beginAtZero: true,
                         stepSize: 1
+                    }
+                }]
+            }
+        }
+    });
+
+    chartPackages.canvas.parentNode.style.height = '400px';
+    chartPackages.canvas.parentNode.style.width = '600px';
+}
+
+function displayStatsDeliveries(stats) {
+
+    if( !stats ) {
+        let json = JSON.stringify({
+            stats: "km",
+            deliveryman: 4
+        }) ;
+        ajax('../api/deliveryManStats/6', json, 'POST', displayStatsDeliveries, console.log) ;
+        return;
+    }
+
+    let kms;
+    let months = [];
+    let numbers = [];
+    try { kms = JSON.parse(stats); }
+    catch (e) { return e }
+    if( kms.length > 12 ) kms = kms.slice(0,11);
+
+    for( let i = 0; i < 12; i++ ) {
+        if( kms[i] ) {
+            months.unshift(Object.keys(kms[i])[0]);
+            numbers.unshift(Object.values(kms[i])[0])
+        }
+    }
+
+    const ctx = document.getElementById('deliveriesCanvas');
+
+    const chartPackages = new Chart(ctx, {
+        type: 'line',
+        responsive: true,
+        data:  {
+            labels: months,
+            datasets: [{
+                label: 'Nb',
+                data: numbers,
+                backgroundColor: 'rgba(153, 102, 255, 0.2)'
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
                     }
                 }]
             }

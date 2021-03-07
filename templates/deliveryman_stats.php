55edd8e1c4aed212daf96bb024b02ec4fb22a5c9
Nigth <omnes.theo@gmail.com>
2021-03-07 15:54:24 +0100

    connect data

--- templates/deliveryman_stats.php
+++ templates/deliveryman_stats.php
@@ -23,39 +23,6 @@
                     <div class="chart-container" style="position: relative; height:40vh; width:80vw">
                         <canvas id="packagesCanvas"></canvas>
                     </div>
-                    <script>
-                        const ctx = document.getElementById('packagesCanvas');
-                        const chartPackages = new Chart(ctx, {
-                            type: 'bar',
-                            responsive: true,
-                            data: {
-                                labels: ['Novembre' ,'Décembre','Janvier', 'Fevrier', 'Mars'],
-                                datasets: [{
-                                    label: ['Nb de colis livrés', 'test'],
-                                    data: [12, 19, 3, 5, 6],
-                                    backgroundColor: [
-                                        'rgba(255, 99, 132, 0.2)',
-                                        'rgba(54, 162, 235, 0.2)',
-                                        'rgba(255, 206, 86, 0.2)',
-                                        'rgba(75, 192, 192, 0.2)',
-                                        'rgba(153, 102, 255, 0.2)'
-                                    ]
-                                }]
-                            },
-                            options: {
-                                scales: {
-                                    yAxes: [{
-                                        ticks: {
-                                            beginAtZero: true
-                                        }
-                                    }]
-                                }
-                            }
-                        });
-
-                        chartPackages.canvas.parentNode.style.height = '400px';
-                        chartPackages.canvas.parentNode.style.width = '600px';
-                    </script>
 
                 </div>
                 <div class="tab-pane fade" id="delivery" role="tabpanel" aria-labelledby="delivery-tab">b</div>

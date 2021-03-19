284693844422912006eb7b8ed7e33eefea567b1c
SarahSch19 <grepo.sarah19@gmail.com>
2021-03-19 21:15:21 +0100

    composer setup

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a67758894007619545a5df8c0b092320751e24f4',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a67758894007619545a5df8c0b092320751e24f4',
+    ),
+  ),
+);

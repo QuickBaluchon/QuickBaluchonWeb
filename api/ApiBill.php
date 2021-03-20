a8d09b720484f11551071bbfb5301aa15aea86eb
SarahSch19 <grepo.sarah19@gmail.com>
2021-03-20 17:02:19 +0100

    removing cats

--- api/ApiBill.php
+++ api/ApiBill.php
@@ -6,76 +6,73 @@ require_once('Api.php');
 class ApiBill extends Api
 {
 
-  private $_method;
-  private $_data = [];
+    private $_method;
+    private $_data = [];
 
-  public function __construct($url, $method)
-  {
+    public function __construct($url, $method)
+    {
 
-    $this->_method = $method;
+        $this->_method = $method;
 
-    if (count($url) == 0)
-      $this->_data = $this->getListBills();     // list of bills - /api/bill
+        if (count($url) == 0)
+            $this->_data = $this->getListBills();     // list of bills - /api/bill
 
-    elseif (($id = intval($url[0])) !== 0)      // details one bills - /api/bill/{id}
-    switch ($method) {
-        case 'GET':$this->_data = $this->getBill($id);break;
-        case 'PATCH':$this->patchBill($id);break;
-        default:
-            // code...
-            break;
+        elseif (($id = intval($url[0])) !== 0)      // details one bills - /api/bill/{id}
+            switch ($method) {
+                case 'GET':$this->_data = $this->getBill($id);break;
+                case 'PATCH':$this->patchBill($id);break;
+                default: $this->catError(405); break;
+            }
+
+
+        echo json_encode($this->_data, JSON_PRETTY_PRINT);
+
     }
 
+    public function getListBills(): array {
+        if($this->_method != 'GET') $this->catError(405);
 
-    echo json_encode($this->_data, JSON_PRETTY_PRINT);
+        if(isset($_GET['client'])) {
+            self::$_where[] = 'client = ?';
+            self::$_params[] = intval($_GET['client']);
+        }
+        if(isset($_GET['paid'])) {
+            self::$_where[] = 'paid = ?';
+            self::$_params[] = intval($_GET['paid']);
+        }
 
-  }
-
-  public function getListBills(): array {
-    if($this->_method != 'GET') $this->catError(405);
-
-    if(isset($_GET['client'])) {
-      self::$_where[] = 'client = ?';
-      self::$_params[] = intval($_GET['client']);
+        $columns = ['id', 'client', 'grossAmount', 'netAmount', 'dateBill', 'pdfPath', 'paid'];
+        $list = $this->get('MONTHLYBILL', $columns);
+        $bills = [];
+        if( $list != null ){
+            foreach ($list as $bill) {
+                $bills[] = $bill;
+            }
+        }
+        return $bills;
     }
-    if(isset($_GET['paid'])) {
-      self::$_where[] = 'paid = ?';
-      self::$_params[] = intval($_GET['paid']);
-    }
 
-    $columns = ['id', 'client', 'grossAmount', 'netAmount', 'dateBill', 'pdfPath', 'paid'];
-    $list = $this->get('MONTHLYBILL', $columns);
-    $bills = [];
-    if( $list != null ){
-      foreach ($list as $bill) {
-        $bills[] = $bill;
-      }
+    public function getBill($id): array
+    {
+        //$this->authentication(['admin'], [$id]);
+        self::$_where[] = 'id = ?';
+        self::$_params[] = $id;
+        $columns = ['id', 'client', 'grossAmount', 'netAmount', 'dateBill', 'pdfPath', 'paid'];
+        $client = $this->get('MONTHLYBILL', $columns );
+        if (count($client) == 1)
+            return $client[0];
+        else
+            return [];
     }
-    return $bills;
-  }
-
-  public function getBill($id): array
-  {
-    if ($this->_method != 'GET') $this->catError(405);
-    //$this->authentication(['admin'], [$id]);
-    self::$_where[] = 'id = ?';
-    self::$_params[] = $id;
-    $columns = ['id', 'client', 'grossAmount', 'netAmount', 'dateBill', 'pdfPath', 'paid'];
-    $client = $this->get('MONTHLYBILL', $columns );
-    if (count($client) == 1)
-      return $client[0];
-    else
-      return [];
-  }
 
     public function patchBill($id){
         $data = $this->getJsonArray();
         $allowed = ['paid'];
 
-         if (count(array_diff(array_keys($data), $allowed)) > 0) {
-             http_response_code(400);
-             exit();
-         }
+        if (count(array_diff(array_keys($data), $allowed)) > 0) {
+            http_response_code(400);
+            exit();
+        }
 
         self::$_set[] = "paid = ?" ;
         self::$_params[] = $data['paid'];

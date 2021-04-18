db555293d39a9c714f15f3e47fad95815cf085a0
Sarah <sschlegel@myges.fr>
2021-04-18 20:24:05 +0200

    case no daily deliveries handled

--- templates/deliveryman_roadmap.php
+++ templates/deliveryman_roadmap.php
@@ -6,75 +6,82 @@ extract($data);
 <div class="container-xl">
     <div class="row">
         <div class="col-lg">
-            <h1><?= $deliveryman['lastname'] ?> / <?= $Title . $roadmap['dateRoute'] ; ?></h1>
+            <?php $Title .= isset($roadmap['dateRoute']) ? $roadmap['dateRoute'] : date('Y/m/d') ; ?>
+            <h1><?= $deliveryman['lastname'] ?> / <?= $Title ?></h1>
 
-            <section class="mb-5 mt-5">
-                <?php
-                foreach ($roadmap['stops'] as $stopNb => $stop):
-                    $style = "";
+            <?php if (count($roadmap) > 0): ?>
+                <section class="mb-5 mt-5">
+                    <?php
+                    foreach ($roadmap['stops'] as $stopNb => $stop):
+                        $style = "";
 
-                    if ($stopNb < $roadmap['currentStop']) {
-                        $style = "text-white " ;
-                        $style .= $stop['delivery'] != null ? "bg-success" : "bg-warning";
-                    }
+                        if ($stopNb < $roadmap['currentStop']) {
+                            $style = "text-white " ;
+                            $style .= $stop['delivery'] != null ? "bg-success" : "bg-warning";
+                        }
 
-                    if ($stopNb == $roadmap['currentStop'])
-                        $style = "bg-primary text-white";
-                ?>
-                    <div class="card mb-3">
-                        <div class="card-header <?= $style ?>">
-                            <?= $CardHeader ?> <?= $stopNb + 1 ?>
+                        if ($stopNb == $roadmap['currentStop'])
+                            $style = "bg-primary text-white";
+                    ?>
+                        <div class="card mb-3">
+                            <div class="card-header <?= $style ?>">
+                                <?= $CardHeader ?> <?= $stopNb + 1 ?>
+                            </div>
+                            <div class="card-body">
+                                <p><?= $TextPackage ?> <?= $stop['package'] ?></p>
+                                <p><?= $stop['address'] ?></p>
+                                <?php
+                                if ($stopNb == $roadmap['currentStop']):
+                                    $hours = intval($stop['timeNextHop']) == 0 ? null : intval($stop['timeNextHop']);
+                                    $minutes = intval($stop['timeNextHop'] * 60) ;
+                                ?>
+                                    <p>
+                                        <?= $TextDistanceToNext ?> <?= $stop['distanceNextHop'] ?> km<br>
+                                        <?= $TextTimeToNext ?> <?php isset($hours) ? $hours . " heures " : "" ; echo $minutes . " minutes" ; ?>
+                                    </p>
+                                <?php endif ; ?>
+                            </div>
+                            <div class="card-footer">
+                                <?php
+                                if ($stop['delivery'] != null):
+                                    echo $TextDateTimeDelivery  . " " . $stop['delivery'] ;
+                                elseif ($stopNb == $roadmap['currentStop']):
+                                    echo $TextDelivering ;
+                                else:
+                                    echo $TextNotDelivered ;
+                                endif; ?>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <p><?= $TextPackage ?> <?= $stop['package'] ?></p>
-                            <p><?= $stop['address'] ?></p>
-                            <?php
-                            if ($stopNb == $roadmap['currentStop']):
-                                $hours = intval($stop['timeNextHop']) == 0 ? null : intval($stop['timeNextHop']);
-                                $minutes = intval($stop['timeNextHop'] * 60) ;
-                            ?>
-                                <p>
-                                    <?= $TextDistanceToNext ?> <?= $stop['distanceNextHop'] ?> km<br>
-                                    <?= $TextTimeToNext ?> <?php isset($hours) ? $hours . " heures " : "" ; echo $minutes . " minutes" ; ?>
-                                </p>
-                            <?php endif ; ?>
+                    <?php endforeach; ?>
+                </section>
+
+                <hr>
+                <?php if ($roadmap['finished'] == 1): ?>
+                <section class="mt-5">
+                    <div class="alert alert-success">
+                        Cette livraison est terminée
+                    </div>
+                </section>
+                <?php else: ?>
+                <section class="mt-5">
+                    <h2><?= $TitleDanger ?></h2>
+                    <div class="container rounded border border-danger d-flex flex-wrap justify-content-between p-4">
+                        <div class="flex-auto">
+                            <strong><?= $LabelDelete ?></strong>
+                            <p class="mb-0"><?= $WarningDelete ?></p>
                         </div>
-                        <div class="card-footer">
-                            <?php
-                            if ($stop['delivery'] != null):
-                                echo $TextDateTimeDelivery  . " " . $stop['delivery'] ;
-                            elseif ($stopNb == $roadmap['currentStop']):
-                                echo $TextDelivering ;
-                            else:
-                                echo $TextNotDelivered ;
-                            endif; ?>
+                        <div>
+                            <button type='button' onclick='updateWarehouse(<?= $id ?>)' class='btn btn-outline-danger'><?= $ButtonDelete ?></button>
                         </div>
                     </div>
-                <?php endforeach; ?>
-            </section>
+                </section>
+                <?php endif; ?>
 
-            <hr>
-            <?php if ($roadmap['finished'] == 1): ?>
-            <section class="mt-5">
-                <div class="alert alert-success">
-                    Cette livraison est terminée
-                </div>
-            </section>
             <?php else: ?>
-            <section class="mt-5">
-                <h2><?= $TitleDanger ?></h2>
-                <div class="container rounded border border-danger d-flex flex-wrap justify-content-between p-4">
-                    <div class="flex-auto">
-                        <strong><?= $LabelDelete ?></strong>
-                        <p class="mb-0"><?= $WarningDelete ?></p>
-                    </div>
-                    <div>
-                        <button type='button' onclick='updateWarehouse(<?= $id ?>)' class='btn btn-outline-danger'><?= $ButtonDelete ?></button>
-                    </div>
-                </div>
-            </section>
+                <section class="mt-5">
+                    <div class="alert alert-info"><?= $TextNoRoadmap ?></div>
+                </section>
             <?php endif; ?>
-
         </div>
     </div>
 </div>

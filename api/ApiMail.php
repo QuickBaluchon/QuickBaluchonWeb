d2df7be235ecd18f8433ada00f7a00f72713873c
nathan <nleoturneau1@myges.fr>
2021-05-06 10:13:34 +0200

    var_dump mail request

--- api/ApiMail.php
+++ api/ApiMail.php
@@ -4,6 +4,6 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
 
 class ApiMail extends Api {
     public function __construct ($email, $subject, $message) {
-        mail($email, $subject, $message);
+        var_dump(mail($email, $subject, $message));
     }
 }

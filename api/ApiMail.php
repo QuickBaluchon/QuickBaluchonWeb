019e80df4c5ba42309c18fe518e89c204dcae21a
nathan <nleoturneau1@myges.fr>
2021-05-06 10:08:56 +0200

    api mail done

--- api/ApiMail.php
+++ api/ApiMail.php
@@ -4,6 +4,6 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php');
 
 class ApiMail extends Api {
     public function __construct ($email, $subject, $message) {
-        var_dump(mail("letourneaunathan40@gmail.com", "test", "coucou c'est moi"));
+        mail($email, $subject, $message);
     }
 }
